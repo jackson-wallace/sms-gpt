ad75dc0b2fe087404f83df8c8c9a92cbe9c9e287
jackson-wallace <82778887+jackson-wallace@users.noreply.github.com>
2023-01-03 14:32:10 -0500

    updated success and cancel urls

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import openai
 import datetime
 import stripe
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, abort, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 import config
@@ -45,13 +45,24 @@ def sms_ahoy_reply():
         user_ref.set({
             'first_text_time': datetime.datetime.now(),
             'subscribed': False,
-            'stripe_customer_id': None
+            'stripe_customer_id': None,
+            'message_count': 1,
         })
 
     # If the user is in the database, retrieve their first text time and subscribed status
     else:
         # Parse the first_text_time string as a timezone-aware datetime object
         subscribed = user['subscribed']
+
+        # Update the 'message_count' field in the database by incrementing it by 1
+        user_ref.update({
+            'message_count': user['message_count'] + 1,
+        })
+
+    # Check if the user is in the database
+    user_ref = db.collection('user_data').document(sender)
+    user = user_ref.get().to_dict()
+    subscribed = user['subscribed']
 
     # If the user's not subscribed, send a message asking them to subscribe
     if not subscribed:
@@ -76,13 +87,16 @@ def sms_ahoy_reply():
             cancel_url=url_for('cancel', _external=True),
         )
 
-        # Include a link to the checkout session in the response text
-        response_text = f"It's been more than a week since your first text! To continue using our service, please subscribe at {checkout_session.url}"
-        # Start our response
-        resp = MessagingResponse()
-        # Add a message
-        resp.message(response_text)
-        return str(resp)
+        response_text = (f"Hi, welcome to sms-gpt! We're excited to have you on board. To get started, you can sign up for your free trial at the link below. This will allow you to ask me unlimited questions for a week. We hope you enjoy using our service! After signing up, I will send you some examples of the types of questions I can answer.{checkout_session.url}\n"
+        )
+
+        message = client.messages.create(
+                    body=response_text,
+                    from_=config.twilio_phone_number,
+                    to=sender
+                )
+
+        return str(message)
 
     """Respond to incoming messages with a receipt SMS."""
     # Use ChatGPT to generate a response
@@ -93,16 +107,22 @@ def sms_ahoy_reply():
         temperature=0.7,
     )
     response_text = response["choices"][0]["text"]
-    # Start our response
-    resp = MessagingResponse()
-    # Add a message
-    resp.message(response_text)
+    message = client.messages.create(
+                    body=response_text,
+                    from_=config.twilio_phone_number,
+                    to=sender
+                )
 
-    return str(resp)
+    return str(message)
 
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/webhook', methods=['GET', 'POST'])
 def handle_webhook():
+
+    # REQUEST TOO BIG
+    if request.content_length > 1024 ** 2:
+        abort(400)
+
     # Extract the request payload
     payload = request.data
 
@@ -112,6 +132,7 @@ def handle_webhook():
     # Try to construct the event using the payload and signature
     try:
         event = stripe.Webhook.construct_event(payload, sig_header, "whsec_EYuk89gAUshXjU1XUnX5tKXVyZzJlP0N")
+
     except Exception as e:
         # If an exception is raised, it means the event could not be constructed
         # This could be due to an invalid payload, signature, or secret key
@@ -124,16 +145,15 @@ def handle_webhook():
     # Convert the payload to a dictionary
     payload_dict = json.loads(payload)
 
-    # Access the customer field
-    customer_id = payload_dict['data']['object']['customer']
-
-    # Retrieve the customer using their ID
-    customer = stripe.Customer.retrieve(customer_id)
-
-    # Extract the phone number from the customer object
-    phone_number = customer['phone']
-
     if event_type == 'customer.subscription.created':
+
+        customer_id = payload_dict['data']['object']['customer']
+
+        # Retrieve the customer using their ID
+        customer = stripe.Customer.retrieve(customer_id)
+
+        # Extract the phone number from the customer object
+        phone_number = customer['phone']
         
         # Update the "subscribed" field in the Firestore database for this customer
         user_ref = db.collection('user_data').document(phone_number)
@@ -142,9 +162,34 @@ def handle_webhook():
             'stripe_customer_id': customer_id
         })
 
+        response_text = (f"Thanks for signing up! Here are some examples of questions that you can ask me:\n"
+            f"\n"
+            f"• What are some healthy recipes for dinner?\n"
+            f"• How do I fix a leaky faucet?\n"
+            f"• How do I create a budget?\n"
+            f"• What are the symptoms of a cold?\n"
+            f"• How do I improve my public speaking skills?\n"
+            f"• What are some good exercises to do at home?\n"
+            f"• Can you summarize this article? (paste link)\n"
+            f"\n"
+            f"I can also assist with more general questions, such as:\n"
+            f"\n"
+            f"• What are some interesting facts about the history of space exploration?\n"
+            f"• What are the major world religions?\n"
+            f"• What are the different types of political systems?\n"
+            f"• What are the causes of the greenhouse effect?\n"
+            f"\n"
+            f"Ask your first question to get started. Have a good one!"
+        )
+
+        message = client.messages.create(
+                    body=response_text,
+                    from_=config.twilio_phone_number,
+                    to=phone_number,
+                )
+        print(message)
+    
     return "", 200
-
-
 
 @app.route('/success')
 def success():
